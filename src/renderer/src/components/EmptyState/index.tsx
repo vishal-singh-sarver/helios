b0d8978727d66ce24a7bb011b33c3b07486876ef
Shikhar Shukla <shikhar.shukla@navyuginfo.com>
2026-04-16 17:46:15 +0530

    dlete buton added

--- src/renderer/src/components/EmptyState/index.tsx
+++ src/renderer/src/components/EmptyState/index.tsx
@@ -7,7 +7,7 @@ interface EmptyStateProps {
 
 function EmptyState({ icon, onCreateNew }: EmptyStateProps): React.JSX.Element {
   return (
-    <div className="flex h-[440px] flex-col items-center justify-center gap-3">
+    <div className="flex h-full flex-col items-center justify-center gap-3">
       <img src={icon} className="h-6 w-6 opacity-80" />
 
       <p className="text-md font-medium text-white">No Projects Found</p>
